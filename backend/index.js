7b075f03ea65c099dfb79338c2445fbd8da5a3ce
nushamy <asamara5@uwo.ca>
2021-03-03 10:02:03 -0500

    Basis for matching

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,26 @@ router.post('/auth', (req, res) => {
 router.post('/login', (req, res, next) => {
   const user = req.body;
 
- /* if (!user.email) {
+  /* if (!user.email) {
+     return res.status(404).send('No email');
+   }
+   if (!user.password) {
+     return res.status(404).send('no password');
+   }
+   return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
+     if (err) {
+       return res.status('404').send('Error')
+     }
+     if (passportUser) {
+       const user = passportUser;
+       user.token = passportUser.generateJWT();
+       return res.status(200).json({ success: true, token: "Bearer " + user.token, email: user.email, category: user.cateogory });
+     }
+     return status(400).info;
+   })(req, res, next);
+ }); */
+  console.log(user.email);
+  if (!user.email) {
     return res.status(404).send('No email');
   }
   if (!user.password) {
@@ -51,34 +70,15 @@ router.post('/login', (req, res, next) => {
     if (err) {
       return res.status('404').send('Error')
     }
+
     if (passportUser) {
       const user = passportUser;
       user.token = passportUser.generateJWT();
-      return res.status(200).json({ success: true, token: "Bearer " + user.token, email: user.email, category: user.cateogory });
+      return res.status(200).json({ success: true, token: "Bearer " + user.token, email: user.email, category: user.category });
     }
     return status(400).info;
   })(req, res, next);
-}); */
-  console.log(user.email);
-   if(!user.email) {
-     return res.status(404).send('No email');
-   }
-   if(!user.password) {
-     return res.status(404).send('no password');
-   }
- return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
-     if(err) {
-      return res.status('404').send('Error')
-     }
-
-     if(passportUser) {
-       const user = passportUser;
-       user.token = passportUser.generateJWT();
-       return res.status(200).json({ success: true, token: "Bearer " + user.token, email: user.email, category: user.category });
-     }
-     return status(400).info;
-   })(req, res, next);
- });
+});
 
 //add a new user to the system and give them an authentication key
 router.post('/register', [
@@ -158,80 +158,97 @@ router.post('/ins-prefer', (req, res, next) => {
   ins_email = req.body.email;
   pref = req.body.preferences;
 
-   Instructor.find({ email: ins_email})
-      .then(items => {
-        if (items.length > 0) {
-          Instructor.updateOne({ email: ins_email}, {preference: pref}, function (err, result) {
-            if (err) {
-              console.log(err)
-              res.status(400).send("Something went wrong")
-            } else {
-              output = {
-                text: "Successfully updated instructor preferences"
-              }
-              res.status(200).send(output)
+  Instructor.find({ email: ins_email })
+    .then(items => {
+      if (items.length > 0) {
+        Instructor.updateOne({ email: ins_email }, { preference: pref }, function (err, result) {
+          if (err) {
+            console.log(err)
+            res.status(400).send("Something went wrong")
+          } else {
+            output = {
+              text: "Successfully updated instructor preferences"
             }
-          })
-        } else {
-          res.status(404).send("Instructor not found")
-        }
-      })
+            res.status(200).send(output)
+          }
+        })
+      } else {
+        res.status(404).send("Instructor not found")
+      }
+    })
 })
 
-router.post('/match', (req, res, next) => {
+router.get('/match', (req, res, next) => {
   //make course array
-  //for loop iterating through course array
-      //course = i of for loop
-      //applicants = search applications for applicants in preferences for matching course;
-      //hiring-array = call hiring prioritization, send course and applicants
-      //app-pref = call applicants, send hiring-array
-      //final-array = call instructors, send course and app-pref
-      //call matching
-  //end for loop
+  Course.find({}, function (err, all_courses) {
+    //for loop iterating through course array
+    //course = i of for loop
+    let all_tas = [];
+    all_courses.forEach(data => {
+      TA.find({ "preference.code": data.code })
+        .then(tas => {
+          //tas = search applications for applicants in preferences for matching course;
+          console.log(data.code)
+          all_tas.push(tas);
+          //hiring-array = call hiring prioritization, send course and applicants
+          //app-pref = call applicants, send hiring-array
+          //final-array = call instructors, send course and app-pref
+          //call matching
+          //end for loop
+
+        })
+        .catch(err => { console.log(err) })
+      
+    });
+
+    setTimeout(() => res.send(all_tas), 1500)
+    
+  });
+
 })
 
 //hiring prioritization
-function hiring(course, applicants){
+function hiring(course, applicants) {
   //sort course by priority (1 is high, 3 is low)
   //get top 50% and return this array
 }
 //applicant preferences
-function applicantPreferences(course, applicants){
+function applicantPreferences(course, applicants) {
   var applicantPref;
 
-   //sort by ranking (1 is high)
-  for(let i=0;i<applicants.length;i++){
-    applicants.sort(function(a,b){
-      return a.preference.rank-b.preference.rank
+  //sort by ranking (1 is high)
+  for (let i = 0; i < applicants.length; i++) {
+    applicants.sort(function (a, b) {
+      return a.preference.rank - b.preference.rank
     });
   }
 
   //get top 50% and return this array
-  let arraysize=applicants.length/2;
+  let arraysize = applicants.length / 2;
 
-  for (let i=0;i<arraysize;i++){
+  for (let i = 0; i < arraysize; i++) {
     applicantPref.push(applicants[i]);
   }
   return applicantPref;
 }
 
 //instructor preferences
-function instructor(course, instructors){
+function instructor(course, instructors) {
 
-var instructorPref;
+  var instructorPref;
   //sort by ranking
-  for(let i=0;i<instructors.length;i++){
-    applicants.sort(function(a,b){
-      return a.preference.rank-b.preference.rank
+  for (let i = 0; i < instructors.length; i++) {
+    applicants.sort(function (a, b) {
+      return a.preference.rank - b.preference.rank
     });
   }
   //send top x
 
-return instructorPref;
+  return instructorPref;
 }
 //matching
-function matching(course, applicants){
-
+function assignTAs(course, applicants) {
+  applicants.forEach(data => (console.log(data)))
 }
 //DATABASE FILLING FUNCTIONS
 //instructor
@@ -247,7 +264,7 @@ router.post('/fillinstructor', (req, res, next) => {
       res.status(400).send(err)
     }
     else {
-      entry = new Instructor({ email: in_email, preference: in_prefer})
+      entry = new Instructor({ email: in_email, preference: in_prefer })
       entry.save(function (err) {
         if (err) {
           console.log(err)
@@ -279,7 +296,7 @@ router.post('/fillta', (req, res, next) => {
       res.status(400).send(err)
     }
     else {
-      entry = new TA({ email: ta_email, name: ta_name, experience: ta_exp, priority: ta_priority, preference: ta_prefer})
+      entry = new TA({ email: ta_email, name: ta_name, experience: ta_exp, priority: ta_priority, preference: ta_prefer })
       entry.save(function (err) {
         if (err) {
           console.log(err)
@@ -309,7 +326,7 @@ router.post('/fillcourse', (req, res, next) => {
       res.status(400).send(err)
     }
     else {
-      entry = new Course({ code: cr_code, instructors: cr_instructors, ta_hours_new: cr_hours})
+      entry = new Course({ code: cr_code, instructors: cr_instructors, ta_hours_new: cr_hours })
       entry.save(function (err) {
         if (err) {
           console.log(err)
@@ -339,7 +356,7 @@ router.post('/fillapps', (req, res, next) => {
       res.status(400).send(err)
     }
     else {
-      entry = new Application({ course: a_course, email: a_email, name: a_name})
+      entry = new Application({ course: a_course, email: a_email, name: a_name })
       entry.save(function (err) {
         if (err) {
           console.log(err)
