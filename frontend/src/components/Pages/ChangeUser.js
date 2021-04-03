7821888fbbcc07ea084a48402d0fc273c407d3c9
Mitchell Zehr <zehrmitch@gmail.com>
2021-04-03 14:27:46 -0400

    Update ChangeUser.js

--- frontend/src/components/Pages/ChangeUser.js
+++ frontend/src/components/Pages/ChangeUser.js
@@ -14,29 +14,58 @@ export default class ManageTA extends Component {
 		};
 	}
 
-	showMatches = () => {
-		document.getElementById("load").style.display = "block";
+    // On input events the states are set to what has been entered
+	handleInputChange = (event) => {
+		const { value, name } = event.target;
+		this.setState({
+			[name]: value,
+		});
+	};
 
-		fetch("http://localhost:3000/api/getcourses", {
-			method: "PUT",
+    onSubmit = (event) => {
+		// Prevent page refresh
+		event.preventDefault();
+		fetch("http://localhost:3000/api/login", {
+			// Creates a post call with the state info
+			method: "POST",
+			body: JSON.stringify(this.state),
 			headers: {
 				"Content-Type": "application/json",
-				"Access-Control-Allow-Origin": "http://localhost:3001",
 			},
 		})
-			.then(async (response) => {
-				const data = await response.json();
-
-				if (!response.ok) {
-					console.log("Error! Fix your Code!");
+			.then((res) => {
+				if (res.status === 200) {
+					// If the response is successful
+					res.json().then(
+						function (data) {
+							// Set all local storage to returned values from DB. Allows them to be accessed from anywhere
+							localStorage.setItem("isAuth", true);
+							localStorage.setItem("token", data.token);
+							localStorage.setItem("email", data.email);
+							localStorage.setItem("category", data.category);
+							// Routes the logged in user to the proper dashboard based on their catagort in the DB
+							if (data.category == "instructor") {
+								// Route to the instructor dashboard
+								this.props.history.push("/instructor-dashboard");
+							} else if (data.category == "admin") {
+								//route to chair dashboard
+								this.props.history.push("/eceadmin-dashboard");
+							} else {
+								//route to ece admin dashboard
+								this.props.history.push("/contact");
+							}
+						}.bind(this)
+					); // Needed to bind this to the non arrow func
+				} else {
+					// If not sucessful
+					const error = new Error(res.error);
+					throw error;
 				}
-
-				this.setState({ message: "Have Data", data: data });
-				document.getElementById("load").style.display = "none";
 			})
-			.catch((error) => {
-				this.setState({ errorMessage: error.toString() });
-				console.log("Error: ", error);
+			.catch((err) => {
+				// Improper email / password handler
+				console.error(err);
+				alert("Please input the your correct password and email");
 			});
 	};
 
