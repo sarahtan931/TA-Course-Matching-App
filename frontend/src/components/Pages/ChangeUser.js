78235dfb8dbe045c4754f21d7703b7f24a209fb0
Kylie Holman <kholma@uwo.ca>
2021-04-03 15:05:32 -0400

    middleware for changing ta

--- frontend/src/components/Pages/ChangeUser.js
+++ frontend/src/components/Pages/ChangeUser.js
@@ -25,7 +25,7 @@ export default class ManageTA extends Component {
     onSubmit = (event) => {
 		// Prevent page refresh
 		event.preventDefault();
-		fetch("http://localhost:3000/api/login", {
+		fetch("http://localhost:3000/api/add_ta", {
 			// Creates a post call with the state info
 			method: "POST",
 			body: JSON.stringify(this.state),
@@ -35,37 +35,14 @@ export default class ManageTA extends Component {
 		})
 			.then((res) => {
 				if (res.status === 200) {
-					// If the response is successful
-					res.json().then(
-						function (data) {
-							// Set all local storage to returned values from DB. Allows them to be accessed from anywhere
-							localStorage.setItem("isAuth", true);
-							localStorage.setItem("token", data.token);
-							localStorage.setItem("email", data.email);
-							localStorage.setItem("category", data.category);
-							// Routes the logged in user to the proper dashboard based on their catagort in the DB
-							if (data.category == "instructor") {
-								// Route to the instructor dashboard
-								this.props.history.push("/instructor-dashboard");
-							} else if (data.category == "admin") {
-								//route to chair dashboard
-								this.props.history.push("/eceadmin-dashboard");
-							} else {
-								//route to ece admin dashboard
-								this.props.history.push("/contact");
-							}
-						}.bind(this)
-					); // Needed to bind this to the non arrow func
+					alert("Successfully changed");
 				} else {
-					// If not sucessful
 					const error = new Error(res.error);
 					throw error;
 				}
 			})
 			.catch((err) => {
-				// Improper email / password handler
 				console.error(err);
-				alert("Please input the your correct password and email");
 			});
 	};
 
